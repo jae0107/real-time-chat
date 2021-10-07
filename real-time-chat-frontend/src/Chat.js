a6f4b1c6fbf81eb6c456a094dbc9b839d74f4bfa
jae0107 <ghjgjh0107@gmail.com>
2021-10-08 02:41:21 +1030

    message

--- real-time-chat-frontend/src/Chat.js
+++ real-time-chat-frontend/src/Chat.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import './Chat.css'
+import ChatHeader from './ChatHeader'
+import AddCircleIcon from '@material-ui/icons/AddCircle'
+import CradGiftcardIcon from '@material-ui/icons/CardGiftcard'
+import GifIcon from '@material-ui/icons/Gif'
+import EmojiEmoticonsIcon from '@material-ui/icons/EmojiEmotions'
+import Message from './Message'
+import { useSelector } from 'react-redux'
+import { selectUser } from './features/userSlice'
+import { selectChannelId, selectChannelName } from './features/appSlice'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import db from './firebase'
+import firebase from 'firebase'
 
 const Chat = () => {
+    const user = useSelector(selectUser)
+    const channelId = useSelector(selectChannelId)
+    const channelName = useSelector(selectChannelName)
+    const [input, setInput] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        if (channelId) {
+            db.collection('channels').doc(channelId).collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+                setMessages(snapshot.docs.map(doc => doc.data()))
+            })
+        }
+
+
+    }, [channelId])
+
+    
+
     return (
-        <div>
+        <div className='chat' >
+            <ChatHeader channelName={channelName} />
+
+            <div className="chat__messages">
+                {messages.map((message) => {
+                    console.log(message)
+                })}
+                {messages.map(message => (
+                    <Message message={message.message} timestamp={message.timestamp} user={message.user} />
+                ))}
+            </div>
+
             
         </div>
     )
