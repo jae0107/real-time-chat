b79c9b6ae74850dcafc5395bd041acbc0a7de78e
jae0107 <ghjgjh0107@gmail.com>
2021-10-14 23:14:47 +1030

    message

--- real-time-chat-frontend/src/Message.js
+++ real-time-chat-frontend/src/Message.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Avatar } from '@material-ui/core'
+import './Message.css'
 
-const Message = () => {
+const Message = ({ timestamp, user, message }) => {
     return (
-        <div>
-            
-        </div>
+        <div className='message' >
+            <Avatar src={user.photo} />
+            <div className="message__info">
+                <h4>{user.displayName}
+                    <span className="message__timestamp">{new Date(timestamp?.toDate()).toUTCString()}</span>
+                </h4>
+
+                <p>{message}</p> 
+            </div>
+        </div >
     )
 }
 
