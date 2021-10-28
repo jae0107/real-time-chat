5788b5aaa28faa7fe43a7247de5a48c0dac79eee
jae0107 <ghjgjh0107@gmail.com>
2021-10-29 01:13:14 +1030

    registerValidSW

--- real-time-chat-frontend/src/serviceWorker.js
+++ real-time-chat-frontend/src/serviceWorker.js
@@ -25,7 +25,7 @@ export function register(config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
         // The URL constructor is available in all browsers that support SW.
         const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-        
+
         if (publicUrl.origin !== window.location.origin) {
             // Our service worker won't work if PUBLIC_URL is on a different origin
             // from what our page is served on. This might happen if a CDN is used to
@@ -55,3 +55,48 @@ export function register(config) {
         });
     }
 }
+
+function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+        .register(swUrl)
+            .then(registration => {
+                registration.onupdatefound = () => {
+                    const installingWorker = registration.installing;
+                    if (installingWorker == null) {
+                        return;
+                    }
+
+                    installingWorker.onstatechange = () => {
+                        if (installingWorker.state === 'installed') {
+                            if (navigator.serviceWorker.controller) {
+                                // At this point, the updated precached content has been fetched,
+                                // but the previous service worker will still serve the older
+                                // content until all client tabs are closed.
+                                console.log(
+                                    'New content is available and will be used when all ' +
+                                    'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                                );
+                
+                                // Execute callback
+                                if (config && config.onUpdate) {
+                                    config.onUpdate(registration);
+                                }
+                            } else {
+                                // At this point, everything has been precached.
+                                // It's the perfect time to display a
+                                // "Content is cached for offline use." message.
+                                console.log('Content is cached for offline use.');
+                
+                                // Execute callback
+                                if (config && config.onSuccess) {
+                                    config.onSuccess(registration);
+                                }
+                            }
+                        }
+                    };
+                };
+            })
+        .catch(error => {
+            console.error('Error during service worker registration:', error);
+        });
+  }
