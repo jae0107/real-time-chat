393c5df9bdfe5c501892f667edbb8afa60d717f3
jae0107 <ghjgjh0107@gmail.com>
2021-10-29 01:15:18 +1030

    unregister

--- real-time-chat-frontend/src/serviceWorker.js
+++ real-time-chat-frontend/src/serviceWorker.js
@@ -131,3 +131,11 @@ function checkValidServiceWorker(swUrl, config) {
         );
     });
 }
+
+export function unregister() {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then(registration => {
+            registration.unregister();
+        });
+    }
+}
