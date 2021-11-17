57c05784ada35415435c9b686de7bdda23b4b148
jae0107 <ghjgjh0107@gmail.com>
2021-11-17 22:51:18 +1030

    firebase

--- real-time-chat-frontend/src/firebase.js
+++ real-time-chat-frontend/src/firebase.js
@@ -1,7 +1,12 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    // your firebase config
+    apiKey: "AIzaSyBde_RMjwWZ5AajHKJ7d90n5y__7SXPLaA",
+    authDomain: "real-time-chat-proj.firebaseapp.com",
+    projectId: "real-time-chat-proj",
+    storageBucket: "real-time-chat-proj.appspot.com",
+    messagingSenderId: "48845551248",
+    appId: "1:48845551248:web:d11b9251b8f4e1129e4a15"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
