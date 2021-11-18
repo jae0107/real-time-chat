207fb66aed7893006546fd8b1fd6f7340604c9df
jae0107 <ghjgjh0107@gmail.com>
2021-11-18 12:25:42 +1030

    mongodb

--- real-time-chat-backend/server.js
+++ real-time-chat-backend/server.js
@@ -11,6 +11,13 @@ app.use(express.json());
 app.use(cors());
 
 // db config
+const mongoURI = 'mongodb+srv://ghjgjh:Jh120301!!@cluster0.vo0sy.mongodb.net/realTimeChat?retryWrites=true&w=majority';
+
+mongoose.connect(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // api routers
 app.get('/', (req, res) => res.status(200).send('hello world'));
